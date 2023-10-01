e0a97aff93b69c0fc71fc011705f1d4536a99d3e
Charles Schleich <schleich.cdaniel@gmail.com>
2023-10-01 13:15:46 +0200

    Edited for desired filepath behaviour

--- main.ts
+++ main.ts
@@ -1,39 +1,51 @@
-import {Platform, Plugin, TFile} from "obsidian";
+import { Platform, Plugin, TFile } from "obsidian";
 import type { MarkdownView } from 'obsidian';
-import {platform} from "os";
+import { platform } from "os";
 
 console.log("Running on sample img")
 export default class HtmlLocalSrcPlugin extends Plugin {
   async onload() {
-	console.log("Running on ---------------onload")
+    console.log("Running on ---------------onload")
     this.registerMarkdownPostProcessor((element, ctx) => {
-		console.log("Running on ===1")
-		const active_file = this.app.workspace.getActiveFile();
-		console.log(active_file.basename);
-      const targetLinks = Array.from(element.getElementsByTagName("img")).filter(
-        (link) =>
-          link.src.contains(active_file.basename)
-      );
-      
-	  
+      console.log("Running on ===1")
+      const active_file = this.app.workspace.getActiveFile();
+      console.log("ActiveFile", active_file.basename);
+
+      let targetLinks = [];
+      Array.from(element.getElementsByTagName("img")).forEach(function (link) {
+        // console.log("Original Link" + link.src)
+        // console.log(link.src.contains(active_file.basename))
+        targetLinks.push(link);
+      }); 
+
+      // const targetLinks = Array.from(element.getElementsByTagName("img")).filter(
+      //   (link) => {
+      //     link.src.contains(active_file.basename)
+      //   }
+      // );
+
       let active_path = this.app.vault.getResourcePath(active_file)
+      console.log('BASE active_file_path: ' + active_path)
       active_path = active_path.substring(0, active_path.lastIndexOf("/"));
       console.log('active_file_path: ' + active_path)
-	  console.log('arg'+targetLinks)
+
+      console.log('targetLinks -' + targetLinks + '-')
       for (const link of targetLinks) {
         let clean_link = link.src.replace('app://obsidian.md/', '')
         // For iOS
         clean_link = clean_link.replace('capacitor://localhost/', '')
         console.log('clean_link: ' + clean_link)
-        let full_link =  active_path + '/' + clean_link
+        let full_link = active_path + '/' + clean_link
+        console.log('original link: ' + link.src)
         console.log('full_link: ' + full_link)
         link.src = full_link
-        if(Platform.isMobile) {
+        
+        if (Platform.isMobile) {
           console.log("Running on mobile platform - setting object fit and height of img")
           link.style.objectFit = "contain"
           link.height = 100
         }
-        }
+      }
     });
   }
 }
